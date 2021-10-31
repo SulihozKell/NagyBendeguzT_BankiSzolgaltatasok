e2a2ad253770d832427392f2aa01afaa77916f49
Nagy Bendegúz <bendeguz@medea.hu>
2021-10-31 16:10:28 +0100

    MegtakaritasiSzamla

---
+++ src/hu/petrik/bankiszolgaltatasok/MegtakaritasiSzamla.java
@@ -0,0 +1,32 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class MegtakaritasiSzamla extends Szamla {
+    private double kamat;
+    private static double alapKamat = 10;
+
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        this.kamat = alapKamat;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    @Override
+    public boolean kivesz(int osszeg) {
+        if (getAktualisEgyenleg() - osszeg >= 0) {
+            aktualisEgyenleg -= osszeg;
+            return true;
+        }
+        return false;
+    }
+
+    public void kamatJovairas() {
+        aktualisEgyenleg *= 1+kamat/100;
+    }
+}
